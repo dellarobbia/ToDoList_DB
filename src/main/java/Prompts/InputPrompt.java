2160dc4b9366fe05235fd7c38cddb3a2b8bcbac8
Andrew <drew.m2340@gmail.com>
2022-10-10 19:27:17 -0400

    Re-added Prompts package to build

---
+++ src/main/java/Prompts/InputPrompt.java
@@ -0,0 +1,51 @@
+package Prompts;
+
+import java.util.Scanner;
+
+/**
+ * Prompt that accepts an input from the user.
+ * @author Andrew McKay
+ */
+public class InputPrompt implements Prompts{
+    //Properties
+    private String prompt;
+    private String userInput;
+
+    //Getters & Setters
+    public String getPrompt() {
+        return prompt;
+    }
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    //Constructor
+    /**
+     * Constructor for InputPrompt w/ a defined prompt.
+     * @param prompt String of text to output to the console to ask for input.
+     */
+    public InputPrompt (String prompt){
+        setPrompt(prompt);
+    }
+
+    //Methods
+    private void retrieveUserInput(Scanner scanner){
+        userInput = scanner.next();
+    }
+
+    /**
+     * Display the prompt and accept an input from the user.
+     */
+    @Override
+    public void displayPrompt() {
+        System.out.println(prompt);
+        retrieveUserInput(new Scanner(System.in));
+    }
+}
---
+++ src/main/java/Prompts/Prompts.java
@@ -0,0 +1,13 @@
+package Prompts;
+
+/**
+ * Interface for classes responsible for displaying prompts to the user
+ * @author Andrew McKay
+ */
+
+public interface Prompts {
+    /**
+     * Displays a prompt to the console.
+     */
+    void displayPrompt();
+}
